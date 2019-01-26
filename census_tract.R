f4fd109855308af840e5d276441b89dfc2f965f8
Francisco <franciscothebrady@gmail.com>
2019-01-26 00:31:32 -0500

    cleaned up census tract script

--- census_tract.R
+++ census_tract.R
@@ -1,13 +1,11 @@
 # finding census block codes and comparing fips codes
 
 # WHAT THIS PROGRAM DOES:
-# 1. reads in 2010-16 storm events
-# 2. builds a loop using the fcc api to return census
-#    block codes for all events.
-# 3. pauses the API request every 1000 calls
-# 4. performs checks on the requests to make sure they're not too far off.
-# 5. saves those checks (because they might be useful later)
-# 6. cbinds the relevant results to the dataset and writes to csv.
+# 1. reads in a df of school locations in California
+# 2. subsets to LAUSD
+# 3. uses the fcc API to retrieve the census tract codes for all the schools
+# 4. turns that API response into a dataframe
+# 5. writes that to a csv.
 
 library(dplyr)
 library(lubridate)
@@ -16,13 +14,10 @@ library(tidyr)
 library(readxl)
 library(purrr)
 
-schools <- read_excel("Documents/schools/pubschls.xlsx", skip = 5)
-test <- schools %>%
+schools <- read_excel("Documents/schools/pubschls.xlsx", skip = 5) %>%
   select(NCESDist, CDSCode, School, Latitude, Longitude) %>%
   filter(NCESDist == "0622710", 
-         Latitude != "No Data") %>%
-  head(12)
-
+         Latitude != "No Data") 
 
 #apply FCC Census Block API from: https://www.fcc.gov/general/census-block-conversions-api
 # https://geo.fcc.gov/api/census/block/find?latitude=38.26&longitude=-77.51&format=json
@@ -45,7 +40,7 @@ request <-
 # run the request
 results <- request %>% lapply(., fromJSON, flatten = TRUE) 
 # turn the returned lists into dfs
-frank <- lapply(results, as.data.frame.list, stringsAsFactors = FALSE)
+tracts <- lapply(results, as.data.frame.list, stringsAsFactors = FALSE)
 # turn the list of dfs into one big old df
 ## (there's gotta be a better way to do this)
-caitlin <- bind_rows(frank)
+lausd_census_tracts <- bind_rows(tracts)
